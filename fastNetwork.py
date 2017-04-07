610ad9dce0198b097cdd159c2c3323f11754d75b
WilliamWickerson <willie7296@gmail.com>
2017-04-07 01:22:25 -0400

    added softmax

--- fastNetwork.py
+++ fastNetwork.py
@@ -7,14 +7,21 @@ def sigmoidFunction(x):
     except OverflowError:
         ret = 0
     return ret
+
+def softMax(array):
+    exp = [numpy.exp(x) for x in array]
+    return numpy.array([x / sum(exp) for x in exp])
         
 global sigmoid
 sigmoid = numpy.vectorize(sigmoidFunction)
 
 class FastNetwork:
-    def __init__(self, rowSizes, learningRate=1):
+    def __init__(self, rowSizes, learningRate=1, softmax=True):
         self.weights = list()
         self.learningRate = learningRate
+        self.softmax = softmax
+        #Rates initialized according  to:
+        #http://datascience.stackexchange.com/questions/10926/how-to-deep-neural-network-weight-initialization
         for i in range(len(rowSizes)):
             if i == 0:
                 r = numpy.sqrt(6 / (1 + rowSizes[i + 1]))
@@ -30,6 +37,7 @@ class FastNetwork:
                 tempArray = numpy.reshape(tempArray, (rowSizes[i], 2))
             self.weights.append(tempArray)
     def startFromFileOld(self, filename):
+        #Open files compatible with network.py's Network
         with open(filename) as weightFile:
             rows = weightFile.readlines()
             rowData = [numpy.fromstring(row.strip()[2:-1], sep=' ') for row in rows]
@@ -42,6 +50,7 @@ class FastNetwork:
                 self.weights[i] = newArray
                 rowData = rowData[size:]
     def startFromFile(self, filename):
+        #Open files and overwrite weights
         with open(filename) as weightFile:
             weightStrings = weightFile.readlines()
             assert len(weightStrings) == len(self.weights)
@@ -52,6 +61,7 @@ class FastNetwork:
                 weightArray = numpy.reshape(weightArray, self.weights[i].shape)
                 self.weights[i] = weightArray
     def writeToFile(self, filename):
+        #Write all of the weights data to file
         with open(filename, 'w') as weightFile:
             numpy.set_printoptions(threshold = numpy.inf, linewidth = numpy.inf)
             for matrix in self.weights:
@@ -59,6 +69,7 @@ class FastNetwork:
                 weightFile.write(numpy.array_str(printable) + "\n")
             numpy.set_printoptions(threshold = 10, linewidth = 75)
     def networkOutputs(self, inputs):
+        #Calculate the outputs for each row in the neural network
         assert len(inputs) == self.weights[len(self.weights) - 1].shape[0]
         outputs = list()
         for i in reversed(range(len(self.weights))):
@@ -75,13 +86,16 @@ class FastNetwork:
             #Otherwise
             else:
                 inputArray = numpy.array(numpy.concatenate((outputs[0], [1])))
-                #inputArray = 
                 #Matrix multiplication of weights and input vector
                 output = self.weights[i] @ inputArray
-                output = sigmoid(output)
+                if i == 0 and self.softmax:
+                    output = softMax(output)
+                else:
+                    output = sigmoid(output)
                 outputs.insert(0, output)
         return outputs
     def classify(self, inputs):
+        #Return the most probable output
         outputs = self.networkOutputs(inputs)
         maxValue = max(outputs[0])
         maxIndex = outputs[0].tolist().index(maxValue)
@@ -92,15 +106,20 @@ class FastNetwork:
         inputs = numpy.array(inputs)
         deltas = list()
         changes = list()
+        #Back propagate the error
         for i in range(len(self.weights)):
-            #Output layer
+            #Output layer error and change
             if i == 0:
-                error = targets - outputs[i]
+                #For some reason softmax flips this
+                if self.softmax:
+                    error = outputs[i] - targets
+                else:
+                    error = targets - outputs[i]
                 delta = error * outputs[i] * (numpy.ones((self.weights[i].shape[0])) - outputs[i])
                 deltas.append(delta)
                 change = numpy.outer((self.learningRate * deltas[i]), numpy.array(numpy.concatenate((outputs[i+1], [1]))))
                 changes.append(change)
-            #Input layer
+            #Input layer error and change
             elif i == len(self.weights) - 1:
                 error = numpy.dot(deltas[i - 1], self.weights[i - 1][:,:-1])
                 delta = error * outputs[i] * (numpy.ones((self.weights[i].shape[0])) - outputs[i])
@@ -109,13 +128,14 @@ class FastNetwork:
                 inputArray = numpy.stack((inputs, numpy.ones(self.weights[i].shape[0])))
                 change = numpy.transpose(doubleDelta * inputArray)
                 changes.append(change)
-            #Hidden layer
+            #Hidden layer error and change
             else:
                 error = numpy.dot(deltas[i - 1], self.weights[i - 1][:,:-1])
                 delta = error * outputs[i] * (numpy.ones((self.weights[i].shape[0])) - outputs[i])
                 deltas.append(delta)
                 change = numpy.outer((self.learningRate * deltas[i]), numpy.array(numpy.concatenate((outputs[i+1], [1]))))
                 changes.append(change)
+        #Update the weights matrices
         for i in range(len(self.weights)):
             self.weights[i] += changes[i]
                 
